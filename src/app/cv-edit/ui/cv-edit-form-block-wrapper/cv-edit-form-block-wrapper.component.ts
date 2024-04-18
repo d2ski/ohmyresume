d3428f018c365b22cd343ecb2584a0bcdd6c501c
d2ski <chukotski@gmail.com>
2024-04-18 15:31:28 +0200

    fix: do not show cv block drag handle while dragging over

--- src/app/cv-edit/ui/cv-edit-form-block-wrapper/cv-edit-form-block-wrapper.component.ts
+++ src/app/cv-edit/ui/cv-edit-form-block-wrapper/cv-edit-form-block-wrapper.component.ts
@@ -1,9 +1,4 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  ViewEncapsulation,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TuiSvgModule } from '@taiga-ui/core';
 import { DragDropModule } from '@angular/cdk/drag-drop';
@@ -15,9 +10,7 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
   templateUrl: './cv-edit-form-block-wrapper.component.html',
   styleUrl: './cv-edit-form-block-wrapper.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  encapsulation: ViewEncapsulation.None,
 })
 export class CvEditFormBlockWrapperComponent {
   @Input({ required: true }) blockTitle!: string;
-  @Input() isDraggable = false;
 }
