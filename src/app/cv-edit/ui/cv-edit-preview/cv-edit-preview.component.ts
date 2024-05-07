2a66003f07a42139863d052980593c5ce34cc6f9
d2ski <chukotski@gmail.com>
2024-05-07 10:27:12 +0200

    feat: add cv page preview

--- src/app/cv-edit/ui/cv-edit-preview/cv-edit-preview.component.ts
+++ src/app/cv-edit/ui/cv-edit-preview/cv-edit-preview.component.ts
@@ -1,5 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+const PAGE_DIMENSIONS = {
+  width: 794,
+  height: 1122,
+  margin: 20,
+} as const;
 
 @Component({
   selector: 'app-cv-edit-preview',
@@ -9,4 +22,22 @@ import { CommonModule } from '@angular/common';
   styleUrl: './cv-edit-preview.component.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CvEditPreviewComponent {}
+export class CvEditPreviewComponent implements OnInit {
+  @ViewChild('container', { static: true })
+  containerView!: ElementRef<HTMLElement>;
+
+  transform = 'transform: scale(1, 1);';
+
+  ngOnInit(): void {
+    this.scaleContainer();
+  }
+
+  @HostListener('window:resize')
+  scaleContainer(): void {
+    const containerHeight = this.containerView.nativeElement.offsetHeight;
+    const scaleFactor =
+      (containerHeight - PAGE_DIMENSIONS.margin) / PAGE_DIMENSIONS.height;
+
+    this.transform = `transform: scale(${scaleFactor}, ${scaleFactor});`;
+  }
+}
