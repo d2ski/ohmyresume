9ab99be8de2bc3e26de59b85e75d0d84d24f4d64
d2ski <chukotski@gmail.com>
2024-06-14 12:06:39 +0200

    feat: add app titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,22 @@ import { Route } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HomeComponent,
+    title: 'Бесплатный онлайн конструктор резюме',
+  },
   {
     path: 'templates',
     loadComponent: () => import('./cv-templates/cv-templates.component'),
+    title: 'Выбор шаблона для резюме | Бесплатный онлайн конструктор резюме',
   },
   {
     path: 'cv/edit',
     loadComponent: () => import('./cv-edit/cv-edit.component'),
+    title:
+      'Редактирование, просмотр и экспорт резюме | Бесплатный онлайн конструктор резюме',
   },
   { path: '**', redirectTo: '/' },
 ];
